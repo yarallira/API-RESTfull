2a120bd576f00d0654543432c140d1ea19522e4f
cbykuser <yarallira555@gmail.com>
2020-02-06 13:43:09 -0300

    Desenvolvimento

---
+++ instagram_clone/api/server.js
@@ -0,0 +1,143 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    multiparty = require('connect-multiparty'),
+    mongodb = require('mongodb'),
+    ObjectId = require('mongodb').ObjectID,
+    fs = require('fs');
+
+var app = express();
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(multiparty());
+
+var port = 3000;
+
+app.listen(port);
+
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
+console.log('servidor HTTP esta escutando na porta ' + port);
+
+app.get('/', function (req, res) {
+    res.send({ msg: 'Olá' });
+});
+
+//Inserir dados no banco Mongodb
+app.post('/api', function (req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    var date = new Date();
+    var time_stamp = date.getTime();
+
+    var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+
+
+    fs.rename(path_origem, path_destino, function (err) {
+        if (err) {
+            res.status(500).json({ erro: err });
+            return;
+        }
+
+        var dados = {
+            url_imagem: url_imagem,
+            titulo: req.body.titulo
+        }
+
+        db.open(function (err, mongoclient) {
+            mongoclient.collection('postagens', function (err, collection) {
+                collection.insert(dados, function (err, records) {
+                    if (err) {
+                        res.json({ status: 'Houve um erro.' });
+                    } else {
+                        res.json({ status: 'Inclusão realizada com sucesso!' });
+                    }
+                    mongoclient.close();
+                });
+            });
+        });
+    })
+});
+
+//Obter dados do banco Mongodb
+app.get('/api', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            collection.find().toArray(function (err, results) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+//Obter dados do banco Mongodb pelo ID
+app.get('/api/:id', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            var id = req.params.id
+
+            console.log('Testando', ObjectId(id));
+            collection.find(ObjectId(id)).toArray(function (err, results) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.status(200).json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+//Atualizar dados
+app.put('/api/:id', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            var id = req.params.id
+            collection.update(
+                { _id: ObjectId(id) },
+                { $set: { titulo: req.body } },
+                {},
+                function (err, records) {
+                    if (err) {
+                        res.json(err);
+                    } else {
+                        res.json(records);
+                    }
+                    mongoclient.close()
+                }
+            );
+        });
+    });
+});
+
+//Deleta dados
+app.delete('/api/:id', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('postagens', function (err, collection) {
+            var id = req.params.id
+            collection.remove({ _id: ObjectId(id) }, function (err, records) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
