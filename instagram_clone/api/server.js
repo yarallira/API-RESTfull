9a64ba40a196a791b18a1e2f6d246f5a141aba21
cbykuser <yarallira555@gmail.com>
2020-02-10 11:19:40 -0300

    finalização

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -2,15 +2,26 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     multiparty = require('connect-multiparty'),
     mongodb = require('mongodb'),
-    ObjectId = require('mongodb').ObjectID,
+    objectId = require('mongodb').ObjectID,
     fs = require('fs');
 
 var app = express();
 
-//body-parser
+// body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(multiparty());
+
+app.use(function (req, res, next) {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+
+});
 
 var port = 3000;
 
@@ -22,29 +33,25 @@ var db = new mongodb.Db(
     {}
 );
 
-console.log('servidor HTTP esta escutando na porta ' + port);
+console.log('Servidor HTTP esta escutando na porta ' + port);
 
 app.get('/', function (req, res) {
     res.send({ msg: 'Olá' });
 });
 
-//Inserir dados no banco Mongodb
+// POST(create)
 app.post('/api', function (req, res) {
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
 
     var date = new Date();
     var time_stamp = date.getTime();
 
     var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
-
     var path_origem = req.files.arquivo.path;
     var path_destino = './uploads/' + url_imagem;
 
-
     fs.rename(path_origem, path_destino, function (err) {
         if (err) {
-            res.status(500).json({ erro: err });
+            res.status(500).json({ error: err });
             return;
         }
 
@@ -57,21 +64,20 @@ app.post('/api', function (req, res) {
             mongoclient.collection('postagens', function (err, collection) {
                 collection.insert(dados, function (err, records) {
                     if (err) {
-                        res.json({ status: 'Houve um erro.' });
+                        res.json({ 'status': 'erro' });
                     } else {
-                        res.json({ status: 'Inclusão realizada com sucesso!' });
+                        res.json({ 'status': 'inclusao realizada com sucesso' });
                     }
                     mongoclient.close();
                 });
             });
         });
-    })
+
+    });
 });
 
-//Obter dados do banco Mongodb
+// GET(ready)
 app.get('/api', function (req, res) {
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
 
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
@@ -87,14 +93,11 @@ app.get('/api', function (req, res) {
     });
 });
 
-//Obter dados do banco Mongodb pelo ID
+// GET by ID(ready)
 app.get('/api/:id', function (req, res) {
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
-            var id = req.params.id
-
-            console.log('Testando', ObjectId(id));
-            collection.find(ObjectId(id)).toArray(function (err, results) {
+            collection.find(objectId(req.params.id)).toArray(function (err, results) {
                 if (err) {
                     res.json(err);
                 } else {
@@ -106,28 +109,35 @@ app.get('/api/:id', function (req, res) {
     });
 });
 
-app.get('/imagens/:imagem', function(req, res){
-    var img = req.params.imagem
+app.get('/imagens/:imagem', function (req, res) {
+    var img = req.params.imagem;
 
-    fs.readFile('./uploads/' +img, function(err, content){
-        if(err){
-            res.status(400).json(err);
+    fs.readFile('./uploads/' + img, function (err, content) {
+        if (err) {
+            res.status(400).json({ err });
             return;
         }
 
-        res.writeHead(200, {'content-type' : 'image/png', 'content-type' : 'image/jpg', 'content-type' : 'image/jpeg'})
+        res.writeHead(200, { 'content-type': 'image/jpg' });
         res.end(content);
-    })
-})
 
-//Atualizar dados
+    });
+});
+
+// PUT by ID(update)
 app.put('/api/:id', function (req, res) {
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
-            var id = req.params.id
             collection.update(
-                { _id: ObjectId(id) },
-                { $set: { titulo: req.body } },
+                { _id: objectId(req.params.id) },
+                {
+                    $push: {
+                        comentarios: {
+                            id_comentario: new objectId(),
+                            comentario: req.body.comentario
+                        }
+                    }
+                },
                 {},
                 function (err, records) {
                     if (err) {
@@ -135,26 +145,35 @@ app.put('/api/:id', function (req, res) {
                     } else {
                         res.json(records);
                     }
-                    mongoclient.close()
+                    mongoclient.close();
                 }
             );
         });
     });
 });
 
-//Deleta dados
+// DELETE by ID(delete)
 app.delete('/api/:id', function (req, res) {
+
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
-            var id = req.params.id
-            collection.remove({ _id: ObjectId(id) }, function (err, records) {
-                if (err) {
-                    res.json(err);
-                } else {
-                    res.json(records);
+            collection.update(
+                {},
+                {
+                    $pull: {
+                        comentarios: { id_comentario: objectId(req.params.id) }
+                    }
+                },
+                { multi: true },
+                function (err, records) {
+                    if (err) {
+                        res.json(err);
+                    } else {
+                        res.json(records);
+                    }
+                    mongoclient.close();
                 }
-                mongoclient.close();
-            });
+            );
         });
     });
 });
